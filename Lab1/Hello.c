4a93d0cfc036dac90d2bf2508379a11535e0755b
dzmoon23 <dzmoon23@gmail.com>
2025-11-08 13:49:00 +0700

    Update lab 1

--- Lab1/Hello.c
+++ Lab1/Hello.c
@@ -1,4 +1,13 @@
-#include <stdio.h>
-int main(){
-    printf("Hello world!");// Xuat ra man hinh "Hello world!"
+#include <stdio.h> /*Khai bao thu vien*/
+int main(){ 	/*Ham main*/
+// printf("Hello world!!"); /* In ra man hinh dong Hello world*/
+	int a,b; /*Khai bao du lieu*/
+	printf("Nhap a va b: ");
+	scanf("%d %d", &a, &b); /* Lay du lieu tu ban phim */
+	printf("Tong cua chuong trinh: %d",a+b);
+    printf("\nHieu cua chuong trinh: %d",a-b);
+	printf("\nBan ten la:");
+	char name[10];
+	scanf("%s", &name);
+	printf("Toi ten la %s",name);
 }
