712f3b6a3cb3712bd7876f55358b7664a915cd9d
dzmoon23 <dzmoon23@gmail.com>
2025-11-09 09:24:19 +0700

    Nop bai lab 3

---
+++ Lab3/Lab3.c
@@ -0,0 +1,119 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+void Thocluc(){
+    float dtb;
+    error:printf("Nhap diem trung binh cua ban: ");
+    scanf("%f",&dtb);
+    if(dtb>=0 && dtb <=10){
+        if(dtb >=9){
+            printf("Chuc mung, ban da dat hoc luc xuat sac\n");
+            system("pause");
+        }
+        else if(dtb >=8 && dtb < 9 ){
+            printf("Dang khen, ban da dat hoc luc gioi\n");
+            system("pause");
+        }
+        else if(dtb >= 6.5 && dtb < 8){
+            printf("Co co gang, ban da dat hoc luc kha\n");
+            system("pause");
+        }
+        else if(dtb >=5 && dtb < 6.5){
+            printf("Dang tiec, ban da dat hoc luc trung binh\n");
+            system("pause");
+        }
+        else{
+            printf("Ca the vuot troi, chuc mung ban dat duoc hoc luc yeu\n");
+            system("pause");
+        }
+    }
+    else{
+        printf("Gia tri khong hop le. Xin vui long nhap lai!!!");
+        system("cls");
+        goto error;
+    }
+}
+void Gptrbacnhat(){
+    float a,b;
+    printf("Phương trình bậc nhất ax + b = 0. Với:\n");
+    printf("a = ");
+    scanf("%f",&a);
+    printf("b = ");
+    scanf("%f",&b);
+    if (a==0){
+        if(b==0){
+            printf("Phương trình có vô số nghiệm");
+        }
+        else if(b!=0){
+            printf("Phương trình vô nghiệm");
+        }
+    }
+    else{
+        printf("Phương trình có một nghiệm: %.2f",-b/a);
+    }
+}
+void Gptrbachai(){
+    float a,b,c,delta;
+    printf("Phương trình bậc hai ax2 + bx + c = 0. Với:\n");
+    printf("a = ");
+    scanf("%f",&a);
+    printf("b = ");
+    scanf("%f",&b);
+    printf("c = ");
+    scanf("%f",&c);
+    if(a == 0){
+        if (b==0){
+            if(c==0){
+            printf("Phương trình có vô số nghiệm");
+            }
+            else if(c!=0){
+            printf("Phương trình vô nghiệm");
+            }
+    }
+    else{
+        printf("Phương trình có một nghiệm: %.2f",-c/b);
+    }
+    }
+    else {
+        delta = pow(b,2) - 4*a*c;
+        if(delta < 0){
+            printf("Phương trình vô nghiệm");
+        }
+        else if (delta == 0){
+
+            printf("Phuong trinh co ngiem kep x = %.2f",-b*(2*a));
+        }
+        else{
+            printf("Phuong trinh co 2 nghiem rieng biet\n");
+            printf("X1 = %.2f \n",(-b + sqrt(delta))/(2*a));
+            printf("X2 = %.2f \n",(-b - sqrt(delta))/(2*a));
+        }
+        
+    }
+}
+int main(){
+    int nhap;
+    printf("\tMenu\n");
+    printf("Chuc nang cua chuong trinh: ");
+    printf("\n1. Tinh hoc luc.");
+    printf("\n2. Giai phuong trinh bac nhat.");
+    printf("\n3. Giai phuong trinh bac hai.");
+    printf("\nChon chuc nang:");
+    scanf("%d",&nhap);
+    switch(nhap){
+        case 1:
+        system("cls");
+        Thocluc();
+        break;
+        case 2:
+        system("cls");
+        Gptrbacnhat();
+        break;
+        case 3:
+        system("cls");
+        Gptrbachai();
+        break;
+        default:
+        break;
+    }
+}
